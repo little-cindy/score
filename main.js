ab3fdb9196415d8f469ba912e47b61e1aae8bae7
little-cindy <28240079@qq.com>
2022-07-29 15:58:10 +0800

    add main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+function disp_alert(){
+    alert("level 1为绿色\nlevel 2为黄色\nlevel 3为橙色\nlevel 4为红色\n对于得分，达到满分的80%为level 1，60%为level 2，30%为level 3，其余为level 4\n对于得分占比，level同得分\n对于班级排名，班级前30%为level 1，前60%为level 2，前80%为level 3，其余为level 4\n对于超越，超越80%为level 1，超越60%为level 2，超越30%为level 3，其余为level 4\n与上一名差距在自身的30%以内为level 1，60%以内为level 2，80%以内为level 3，其余为level 4\n与下一名差距在自身的30%以内为level 4，60%以内为level 3，80%以内为level 2，其余为level 1")
+}
